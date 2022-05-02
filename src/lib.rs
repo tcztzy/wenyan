c3815ecb45d7d5fd7930284db82999184386ad4b
Tang Ziya <tcztzy@hotmail.com>
2022-05-02 19:14:58 +0800

    REFACTOR: in Rust

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(feature = "pyo3")]
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[cfg(feature = "pyo3")]
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[cfg(feature = "pyo3")]
+#[pymodule]
+fn wenyan(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
