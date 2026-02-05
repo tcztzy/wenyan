d95a2e66e264f9178e3629e6b514f4083c5585e9
Tang Ziya <tcztzy@gmail.Com>
2026-02-05 10:36:34 +0800

    Remove legacy package implementation

--- wenyan/__main__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from wenyan.cmdline import wenyan_main
-
-# Running wenyan as a module (e.g. `python -m wenyan`)
-# is equivalent to running the main `wenyan` command.
-
-sys.exit(wenyan_main())
